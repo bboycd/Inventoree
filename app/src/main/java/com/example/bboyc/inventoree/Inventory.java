d15d66dc68675fd03a36b9e4157c51ff92907216
Chris Rohr <chrisdrohr@gmail.com>
2016-09-05 15:38:48 -0700

    working on database-inventoryclass

--- app/src/main/java/com/example/bboyc/inventoree/Inventory.java
+++ app/src/main/java/com/example/bboyc/inventoree/Inventory.java
@@ -1,7 +1,48 @@
 package com.example.bboyc.inventoree;
 
 public final class Inventory {
-    private Inventory() {
+
+    int id;
+    String name;
+    String detail;
+    public Inventory() {
     }
+
+    public Inventory(int id, String name, String detail) {
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
+    }
+
+    public Inventory(String name, String detail) {
+        this.name = name;
+        this.detail = detail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+
 }
 
